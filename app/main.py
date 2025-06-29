3cfb8245ba3303c3cb63482e12eb3252c664ad2e
viannik <ivan.nikitski@gmail.com>
2025-06-29 16:34:16 +0300

    Add skill learning in FullStackDeveloper initialization

--- app/main.py
+++ app/main.py
@@ -44,6 +44,8 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        for skill in ["Python", "SQL", "Django"]:
+            self.learn_skill(skill)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
